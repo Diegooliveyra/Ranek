d131ab9ebf307bda2534a62e1abd7cd780647af7
Diegooliveyra <diegooliveirabryto@gmail.com>
2021-03-06 19:35:50 -0300

    configurado o head

--- src/pages/Produto/Produto.js
+++ src/pages/Produto/Produto.js
@@ -1,31 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import Head from '../../Components/Head/Head';
 import '../../style/GlobalStyle.css';
 import './Produto.css';
 
 const Produto = () => {
   const params = useParams();
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    fetch(`https://ranekapi.origamid.dev/json/api/produto/${params.id}`)
-      .then((response) => response.json())
-      .then((json) => setData(json));
+    const fetchProdutos = async () => {
+      setLoading(true);
+      const response = await fetch(
+        `https://ranekapi.origamid.dev/json/api/produto/${params.id}`,
+      );
+      const json = await response.json();
+      setData(json);
+      setLoading(false);
+    };
+    fetchProdutos();
   }, [params]);
-  console.log(data.fotos);
   return (
     <div className="container produtos">
+      <Head title={`RanekS | ${data.nome}`} />
+      {loading && <h1>Loading</h1>}
       <div className="images">
         {data.fotos &&
           data.fotos.map((foto) => (
             <img key={foto.src} src={foto.src} alt="" />
           ))}
       </div>
-      <div className="content">
-        <h2>{data.nome}</h2>
-        <span>R$ {data.preco}</span>
-        <p>{data.descricao}</p>
-      </div>
+      {!loading && (
+        <div className="content">
+          <h2>{data.nome}</h2>
+          <span>R$ {data.preco}</span>
+          <p>{data.descricao}</p>
+        </div>
+      )}
     </div>
   );
 };
